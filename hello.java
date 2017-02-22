9d134b86dfc4b057990633580b2b575d679a93f7
kannanmurthy <kannansm10@gmail.com>
2017-02-22 12:19:27 -0800

    cleaned up

--- hello.java
+++
@@ -1,5 +0,0 @@
-System.out.println("Hello World!");
-
-
-System.out.println("I am not here");
-System.out.println("Ignore this file because I was using it to learn gitHub. I suggest y'all learn it too.");
